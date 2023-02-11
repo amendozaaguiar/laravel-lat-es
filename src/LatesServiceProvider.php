3aade6ae8f231c2446af2871c67c2b5799756b1b
Anderson Mendoza <amendoza@inmov.com>
2023-02-10 21:11:21 -0500

    traducción

---
+++ src/LatesServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Amendozaaguiar\Lates;
+
+use Illuminate\Support\ServiceProvider;
+
+class LatesServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([__DIR__ . '/../resources/lang' => base_path('/Lang'),], 'lang');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                Commands\InstallLates::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
