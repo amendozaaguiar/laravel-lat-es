39f495e1c7760381d447b4f1ec89e1e76cd747a4
Anderson Mendoza <amendoza@inmov.com>
2023-02-10 22:05:19 -0500

    traducción a la versión 9 de laravel

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -1,6 +1,13 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Líneas de traducción de autenticación
+    |--------------------------------------------------------------------------
+    */
+
     'failed'   => 'Estas credenciales no coinciden con nuestros registros.',
     'password' => 'La contraseña es incorrecta.',
     'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
